0a7c888b6656154fa6516b17acf99bd2e6d3b5b5
chisschwartz <christopherschwartz559@gmail.com>
2024-05-21 17:58:40 -0500

    passes 7-10

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,26 +3,30 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName = ""
+const candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first American woman in space? "
-let correctAnswer = "Sally Ride"
-let candidateAnswer = ""
+const question = "Who was the first American woman in space? "; 
+const correctAnswer = "Sally Ride";
+const candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+const questions = ["Who was the first American woman in space? " , "True or false: 5 kilometer == 5000 meters? " , "(5 + 3)/2 * 10 = ? " , "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? " , "What is the minimum crew size for the ISS? "];
+const correctAnswers = ["Sally Ride" , "true" , "40" , "Trajectory" , "3"];
+const candidateAnswers = "";
 
 
-function askForName(candidateName) {
+function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-input(candidateName);
+candidateName = input.question("What is your name?");
 }
 
-function askQuestion(question) {
+function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+
+for (let index = 0; index < questions.length; index++) {
+  candidateAnswers = input.question([index]);
+}
 
 
 }
